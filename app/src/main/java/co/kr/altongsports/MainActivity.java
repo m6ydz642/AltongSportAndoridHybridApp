c3441da0c3dbf32a62424d2af8b155fabfcb0695
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-12-03 14:17:14 +0900

    뒤로가기, 뷰내 이동 추가

--- app/src/main/java/co/kr/altongsports/MainActivity.java
+++ app/src/main/java/co/kr/altongsports/MainActivity.java
@@ -23,14 +23,44 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                /*
+                * 처음 엑티비티에 로드할 경우에는 문제가 없지만 WebView 상에서 페이지 이동이 일어날 겨우 보통 디바이스의 브라우저로 실행된다.
+        이 경우에는 WebViewClient 의 shouldOverrideUrlLoading 함수를 사용하여 아주 간단하게 해결된다.
+        쉽게 말해서 loadUrl 로 호출하던 방법을 WebViewClient 에서 대신 호출한다고 생각하면 된다.
+        https://cofs.tistory.com/186 [CofS]
+                *
+                *
+                * */
+                view.loadUrl(url);
+                return true;
+            }
+        });
 
         mWebView.loadUrl("https://altongsports.com/");
 
 
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) { // 뒤로가기 이벤트
 
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            if (mWebView.canGoBack()) {
+
+                mWebView.goBack();
+
+                return false;
+
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
 
 
 
